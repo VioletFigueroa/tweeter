aa92e1a6c564fde495938910172af28f010d2b0c
VioletFigueroa <Permafry_42@hotmail.com>
2021-03-26 20:38:04 +0000

    Adds styling and HTML changes

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+$(document).ready(function() {
+  $(".new-tweet").on("keyup", "textarea", function() {
+    const counter = $(this).parent().find(".counter");
+    let charRemaining = 140 - this.textLength;
+    counter.text(charRemaining);
+    charRemaining < 0
+      ? counter.css("color", "#ff0000")
+      : counter.css("color", "#000000");
+  });
+});
